5a073cfaa67832729a62c9ec0be0444dec2d0f02
Tania <taryutova@yandex.ru>
2022-11-15 13:37:31 +0300

    added task

--- 07_1_regexpo/src/js/__tests__/app.test.js
+++ 07_1_regexpo/src/js/__tests__/app.test.js
@@ -1,26 +1,35 @@
 import Validator from '../app';
 
-test('Проверка пройдена', () => {
+test('Валидное имя (пройдено); только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)', () => {
   const received = new Validator().validateUsername('yes-12ngh_jh1l');
-  expect(received).toBe('yes-12ngh_jh1l');
-}); // тест не пройден
+  console.log(received);
+  expect(received).toEqual('yes-12ngh_jh1l');
+});
 
-test('Недопустимые символы', () => {
-  const received = new Validator().validateUsername('yes-1выа2ngh_jh1l');
-  expect(received).toThrow('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
-}); // тест не пройден
+test('Валидное имя (ОШИБКА); только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)', () => {
+  expect(() => {
+    const received = new Validator().validateUsername('yes-12ngh_jh1l$');
+    console.log(received);
+  }).toThrow('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
+});
 
-test('Более трех цифр подряд', () => {
-  const received = new Validator().validateUsername('yes-1234ngh_jh1l');
-  expect(received).toBe(true);
-}); // тест не пройден
+test('Более трех цифр подряд (ОШИБКА)', () => {
+  expect(() => {
+    const received = new Validator().validateUsername('yes-1234ngh_jh1l');
+    console.log(received);
+  }).toThrow('Имя не должно содержать подряд более трёх цифр');
+});
 
-test('Начало с неверного символа', () => {
-  const received = new Validator().validateUsername('5yes-12ngh_jh1l');
-  expect(received).toBe(true);
-}); // тест не пройден
+test('Начало с неверного символа (ОШИБКА)', () => {
+  expect(() => {
+    const received = new Validator().validateUsername('5yes-12ngh_jh1l');
+    console.log(received);
+  }).toThrow('Имя не должно начинаться цифрами, символами подчёркивания или тире');
+});
 
-test('Оканчивается на неверный символ', () => {
-  const received = new Validator().validateUsername('yes-12ngh_jh1l5');
-  expect(received).toBe(true);
-}); // тест не пройден
+test('Начало с неверного символа (ОШИБКА)', () => {
+  expect(() => {
+    const received = new Validator().validateUsername('yes-12ngh_jh1l5');
+    console.log(received);
+  }).toThrow('Имя не должно заканчиваться цифрами, символами подчёркивания или тире');
+});
--- 07_1_regexpo/src/js/app.js
+++ 07_1_regexpo/src/js/app.js
@@ -1,9 +1,9 @@
 export default class Validator {
   validateUsername(name) {
     this.name = name;
-    if (/^[a-z\d_-]$/.test(name) === false) {
+    if (/^[A-Za-z0-9_-]+$/.test(name) === false) {
       throw new Error('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
-    } else if (/\d{4,}/.test(name) === true) {
+    } else if (/[0-9]{4,}/.test(name) === true) {
       throw new Error('Имя не должно содержать подряд более трёх цифр');
     } else if (/^[\d_-]/.test(name) === true) {
       throw new Error('Имя не должно начинаться цифрами, символами подчёркивания или тире');
