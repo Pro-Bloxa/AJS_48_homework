ba23042006e873701e75fc06ded321276808c02d
Tania <taryutova@yandex.ru>
2022-11-21 11:01:16 +0300

    added task

---
+++ 08_1_containers/src/js/app.js
@@ -0,0 +1,21 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Персонаж уже существует в команде!');
+    }
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => this.members.add(character));
+  }
+
+  toArray() {
+    // console.log([...this.members]);
+    return [...this.members];
+  }
+}
